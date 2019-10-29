f8b17d69247164a4d022bceba97315848f9c2b68
housseine <housseine2014tassa@gmail.com>
2019-10-29 11:24:22 +0100

    kick off

---
+++ src/main/java/com/housseine/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.housseine.springsecurityjwt.models;
+
+public class AuthenticationResponse {
+	private String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		super();
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
+
+}
